62b86af10ea13257a2cd1bdda395c6bd738e00c7
amitbiochem04 <amitbiochem04@gmail.com>
2017-05-11 10:15:18 +0200

    Update plot.R

--- plot.R
+++ plot.R
@@ -5,20 +5,21 @@ library(tidyr)
 #####plot all ressignificant gene in one pdf file 
 
 pdf('light_inducibale_gene.pdf')
-p<- data.frame(colData(fullData),t(counts(fullData, normalized=TRUE)[rownames(ressig),][i] ) ) %>%
+p<- data.frame(colData(fullData),t(counts(fullData, normalized=TRUE)[rownames(ressig),][i]))[-58,]%>%
 gather( "gene", "ncount", -(1:5) ) %>%
 ggplot + geom_line(aes( x=time, y=log2(ncount+1), col=samplelabel)) +facet_wrap(~gene)
 p<- p + xlab( "Time[h]") + ylab("Log2(count)+1") + theme_bw()
+p<-p+geom_point(aes(x=time,y=log2(ncount+1),col=samplelabel))
 facet_multiple( plot = p, facets = 'gene', ncol = 4, nrow = 4)
 
 ######plot single gene 
 
-p<-data.frame( colData(fullData),t(counts(fullData, normalized=TRUE)["NCU00552", , drop=FALSE] ) ) %>%
+p<-data.frame( colData(fullData),t(counts(fullData, normalized=TRUE)["NCU00552", , drop=FALSE]))[-58,]%>%
 gather( "gene", "ncount", -(1:5) )%>%
 ggplot +geom_line(aes(x=time, y=log2(ncount+1), col=samplelabel)) + facet_wrap(~gene)
 p+ xlab("Time[h]") + ylab("Log2(count)+1") + theme_bw()
 
-###### plot in one gene in one png file
+###### plot in one gene in one png file, removed one sample for fulldata
 for( i in row.names(ressig))
       {
 p<-data.frame(colData(fullData),t(counts(fullData, normalized=TRUE)[i, , drop=FALSE]) )[-58,] %>%
@@ -27,7 +28,7 @@ ggplot +geom_line(aes(x=time, y=log2(ncount+1), col=samplelabel)) + facet_wrap(~
 p<- p+ xlab("Time[h]") + ylab("Log2(count)+1")
 p<-p+theme_bw()
 p<-p+geom_point(aes(x=time,y=log2(ncount+1),col=samplelabel))
-p<-p+ labs(title = "i")
+#p<-p+ labs(title = "i")
 ggsave(paste('~/Desktop/Light-dark/plot/',i , '.png', sep=''), p)
 #print(i)
 }
