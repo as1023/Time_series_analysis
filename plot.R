eb75fd443d512b88650b3aafe8c4a54b66ba68d9
amitbiochem04 <amitbiochem04@gmail.com>
2017-05-16 12:00:20 +0200

    Update plot.R

--- plot.R
+++ plot.R
@@ -19,16 +19,21 @@ p+ xlab("Time[h]") + ylab("Log2(count)+1") + theme_bw()
 
 ###### plot in one gene in one png file, removed one sample for fulldata
 for( i in row.names(ressig))
-      {
-p<-data.frame(colData(fullData),t(counts(fullData, normalized=TRUE)[i, , drop=FALSE]) )[-58,] %>%
-gather( "gene", "ncount", -(1:5) )%>%
-ggplot +geom_line(aes(x=time, y=log2(ncount+1), col=samplelabel)) + facet_wrap(~gene)
-p<- p+ xlab("Time[h]") + ylab("Log2(count)+1")
-p<-p+theme_bw()
-p<-p+geom_point(aes(x=time,y=log2(ncount+1),col=samplelabel))
-#p<-p+ labs(title = "i")
-ggsave(paste('~/Desktop/Light-dark/plot/',i , '.png', sep=''), p)
-#print(i)
+ {
+  p<-data.frame(colData(fullData),t(counts(fullData, normalized=TRUE)[i, , drop=FALSE]) )[-58,] %>%
+    gather( "gene", "ncount", -(1:5) )%>%
+    ggplot +geom_line(aes(x=time, y=log2(ncount+1), col=samplelabel)) + facet_wrap(~gene)
+  p<- p+ xlab("Time[h]") + ylab("Log2(count)+1")
+  p<-p+theme_bw()+coord_fixed(ratio=.5)
+  p<-p+geom_point(aes(x=time,y=log2(ncount+1),col=samplelabel))
+  p<-p+ scale_x_continuous(breaks = c(0,2, 4, 6,8,10,12,14,16,18,20,22)) 
+  p<-p+ ylim(0,16)
+ p<-p+theme(axis.text = element_text (face="bold", size=14), axis.title=element_text(size=18,face="bold"))
+ p<-p+theme(legend.title = element_text(size=16, face="bold"),legend.text = element_text(size = 16, face = "bold"))
+ p<-p+theme(strip.text = element_text(size=25,face="bold"))
+ #theme(legend.title = element_text(colour="blue", size=16, face="bold"))
+ggsave(paste('~/Desktop/Light-dark/images/',i , '.png', sep=''), width=20, height=15, p)
+  print(i)
 }
 
 
